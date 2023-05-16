2a47edda0cd47f4d9f95ba3163cc1b4b583a3aed
Nerpassevera <63259752+Nerpassevera@users.noreply.github.com>
2023-05-16 16:08:44 -0700

    Update 028_Map_and_Lambda_Function.py

--- 028_Map_and_Lambda_Function.py
+++ 028_Map_and_Lambda_Function.py
@@ -1,16 +1,12 @@
 # The "Map and Lamda Functions": https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
-
 cube = lambda x: x ** 3  # complete the lambda function
 
 
 def fibonacci(n):
     """Return a Fibonacci sequence with n components"""
-    fibonacci_list = []
-    for i in range(n):
-        if len(fibonacci_list) > 1:
-            fibonacci_list.append(fibonacci_list[-1]+fibonacci_list[-2])
-        else:
-            fibonacci_list.append(i)
+    fibonacci_list = [0, 1, 1]
+    while len(fibonacci_list) < n:
+        fibonacci_list.append(fibonacci_list[-1]+fibonacci_list[-2])
     # return a list of fibonacci numbers
     return fibonacci_list
 if __name__ == '__main__':
